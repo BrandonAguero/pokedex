a8a06e391d07f13e9cb6c243b860acf1c8eefc7b
Brandon <name.brandon.aguero@gmail.com>
2023-06-18 00:07:29 -0500

    Agregue nuevas rutas y modifique al pokedex

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -4,9 +4,13 @@ import { useSelector } from "react-redux";
 import PokeContainer from "../components/Pokedex/PokeContainer.jsx";
 import { useNavigate } from "react-router";
 import axios from "axios";
+import Select from "react-select";
 
 const Pokedex = () => {
-  const [selectValue, setSelectValue] = useState("all-pokemons");
+  const [selectedOption, setSelectedOption] = useState({
+    value: "all-pokemons",
+    label: "Todos los pokemones",
+  });
 
   const trainerName = useSelector((states) => states.trainerName);
 
@@ -17,12 +21,24 @@ const Pokedex = () => {
   const [allInfo, getApiPoke, hasError, setAllInfo] = useFetch(url);
   const [types, getAllTypes] = useFetch(urlTypes);
 
+  const options = types?.results.map((type) => {
+    const name = type.name;
+    return {
+      value: type.url,
+      label: name.charAt(0).toUpperCase() + name.slice(1),
+    };
+  });
+  options?.unshift({
+    value: "all-pokemons",
+    label: "Todos los pokemones",
+  });
+
   useEffect(() => {
-    if (selectValue === "all-pokemons") {
+    if (selectedOption.value === "all-pokemons") {
       getApiPoke();
     } else {
       axios
-        .get(selectValue)
+        .get(selectedOption.value)
         .then((res) => {
           const data = {
             results: res.data.pokemon.map((pokeInfo) => pokeInfo.pokemon),
@@ -31,7 +47,7 @@ const Pokedex = () => {
         })
         .catch((err) => console.log(err));
     }
-  }, [selectValue]);
+  }, [selectedOption]);
 
   useEffect(() => {
     getAllTypes();
@@ -40,36 +56,85 @@ const Pokedex = () => {
   const searchPokemon = useRef();
   const navigate = useNavigate();
 
-  console.log(types);
-
   const handleSubmit = (e) => {
     e.preventDefault();
     const inputValue = searchPokemon.current.value.trim();
     navigate(`/pokedex/${inputValue}`);
   };
 
-  const handleChangeType = (e) => {
-    setSelectValue(e.target.value);
+  const handleChange = (selectedOption) => {
+    setSelectedOption(selectedOption);
   };
 
   return (
     <>
-      <div>Pokedex</div>
-      <h3>
-        Bienvenido {trainerName}, aquí podrás encontrar tu pokemón favorito
-      </h3>
-      <form onSubmit={handleSubmit}>
-        <input ref={searchPokemon} type="text" />
-        <button>Search</button>
-        <select onChange={handleChangeType}>
-          <option value="all-pokemons">Todos los pokemones</option>
-          {types?.results.map((typeInfo) => (
-            <option key={typeInfo.url} value={typeInfo.url}>
-              {typeInfo.name}
-            </option>
-          ))}
-        </select>
-      </form>
+      <header className="row-start-1 row-end-7 flex items-center justify-center">
+        <div className="flex w-nine flex-col items-center gap-8 lg:gap-16">
+          <figure className="max-w-[54rem]">
+            <img src="./src/assets/png/logo-main.png" alt="Logo principal" />
+          </figure>
+          <div>
+            <h2 className="text-center font-inter text-2xl text-titleRed lg:text-4xl">
+              Bienvenido {trainerName},{" "}
+              <span className="text-paragraph">
+                aquí podrás encontrar tu pokemón favorito
+              </span>
+            </h2>
+          </div>
+          <form
+            className="flex w-full flex-col items-center gap-4 lg:flex-row lg:justify-center lg:gap-8"
+            onSubmit={handleSubmit}
+          >
+            <div className="font-roboto">
+              <input
+                className="phone:w-[26rem] w-[22rem] p-4 text-2xl shadow-lg placeholder:text-2xl placeholder:font-medium focus:outline-button sm:w-[35rem] lg:p-8 lg:text-4xl lg:placeholder:text-4xl"
+                ref={searchPokemon}
+                type="text"
+                placeholder="Busca un pokemón"
+              />
+              <button className="bg-button p-4 text-2xl text-white lg:p-8 lg:text-4xl">
+                Search
+              </button>
+            </div>
+            <div className="w-[21rem]  font-roboto text-2xl shadow-lg lg:h-[6.5rem] lg:w-[32rem] lg:p-6 lg:text-4xl">
+              <Select
+                options={options}
+                value={selectedOption}
+                onChange={handleChange}
+                isSearchable={false}
+                styles={{
+                  control: (provided) => ({
+                    ...provided,
+                    border: "none",
+                    boxShadow: "none",
+                  }),
+                  indicatorSeparator: () => ({
+                    display: "none",
+                  }),
+                  option: (provided, state) => ({
+                    ...provided,
+                    cursor: "pointer",
+                    ":hover": {
+                      background: "#ED8F8F",
+                      color: "white",
+                    },
+                    background: state.isSelected ? "#ED8F8F" : "transparent",
+                    color: state.isSelected ? "white" : "black",
+                  }),
+                }}
+                theme={(theme) => ({
+                  ...theme,
+                  borderRadius: 0,
+                  colors: {
+                    ...theme.colors,
+                    primary25: "#ED8F8F",
+                  },
+                })}
+              />
+            </div>
+          </form>
+        </div>
+      </header>
       <PokeContainer pokemons={allInfo?.results} />
     </>
   );
